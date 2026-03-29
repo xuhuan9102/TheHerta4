12bb8ea636fb473da8193dc9dcd239ba647691bb
Administrator <Administrator@DESKTOP-BBNRNSR>
2026-03-29 17:50:35 +0800

    op

--- games/yysls.py
+++ games/yysls.py
@@ -13,23 +13,31 @@ class ExportYYSLS(DrawIBExportBase):
     def __init__(self, blueprint_model):
         super().__init__(blueprint_model=blueprint_model, combine_ib=False)
 
+    @staticmethod
+    def _get_submesh_ib_resource_name(submesh_model) -> str:
+        return "Resource_" + submesh_model.unique_str.replace("-", "_") + "_Index"
+
     def add_unity_vs_texture_override_ib_sections(self, ini_builder: M_IniBuilder, drawib_model):
         texture_override_ib_section = M_IniSection(M_SectionType.TextureOverrideIB)
         draw_ib = drawib_model.draw_ib
         d3d11_game_type = drawib_model.d3d11GameType
+        part_name_by_unique_str = {
+            submesh_model.unique_str: part_name
+            for part_name, submesh_model in drawib_model.part_name_submesh_dict.items()
+        }
 
-        for count_i, part_name in enumerate(drawib_model.part_name_list):
-            match_first_index = str(drawib_model.match_first_index_list[count_i])
-            style_part_name = "Component" + part_name
-            texture_override_name_suffix = "IB_" + draw_ib + "_" + drawib_model.draw_ib_alias + "_" + style_part_name
-            ib_resource_name = drawib_model.PartName_IBResourceName_Dict.get(part_name, "")
+        for submesh_model in drawib_model.submesh_model_list:
+            match_first_index = str(submesh_model.match_first_index)
+            texture_override_name_suffix = submesh_model.unique_str.replace("-", "_")
+            ib_resource_name = self._get_submesh_ib_resource_name(submesh_model)
 
             texture_override_ib_section.append("[TextureOverride_" + texture_override_name_suffix + "]")
             texture_override_ib_section.append("hash = " + draw_ib)
             texture_override_ib_section.append("match_first_index = " + match_first_index)
+            texture_override_ib_section.append("match_index_count = " + str(submesh_model.match_index_count))
             texture_override_ib_section.append("handling = skip")
-
-            ib_buf = drawib_model.componentname_ibbuf_dict.get("Component " + part_name, None)
+            
+            ib_buf = drawib_model.submesh_ib_dict.get(submesh_model.unique_str, None)
             if ib_buf is None or len(ib_buf) == 0:
                 texture_override_ib_section.append("ib = null")
                 texture_override_ib_section.new_line()
@@ -42,6 +50,7 @@ class ExportYYSLS(DrawIBExportBase):
             texture_override_ib_section.append("ib = " + ib_resource_name)
 
             if not GlobalProterties.forbid_auto_texture_ini():
+                part_name = part_name_by_unique_str.get(submesh_model.unique_str)
                 texture_markup_info_list = drawib_model.partname_texturemarkinfolist_dict.get(part_name, None)
                 if texture_markup_info_list is not None:
                     for texture_markup_info in texture_markup_info_list:
@@ -54,13 +63,17 @@ class ExportYYSLS(DrawIBExportBase):
                         category_original_slot = d3d11_game_type.CategoryExtractSlotDict[original_category_name]
                         texture_override_ib_section.append(category_original_slot + " = Resource" + draw_ib + original_category_name)
 
-            submesh_model = drawib_model.part_name_submesh_dict.get(part_name)
-            if submesh_model is not None:
-                for drawindexed_str in M_IniHelper.get_drawindexed_str_list(
-                    submesh_model.drawcall_model_list,
-                    obj_name_draw_offset_dict=drawib_model.obj_name_draw_offset,
-                ):
-                    texture_override_ib_section.append(drawindexed_str)
+            # TODO 这里注意，YYSLS大世界中使用的是DrawindexedInstancedIndirect
+            # 第一个参数是一个专门的参数Buffer，但是我们目前还没发构造这个参数Buffer，所以要加上
+            # 第二个参数是Buffer的偏移量，因为一般是一个巨大的Buffer包含了这一帧所有要绘制的内容
+            # 但是角色外观界面，使用的是DrawIndexed
+            # 所以这个能够兼容的方法仍然需要摸索，也许能够通过某种DRAW_TYPE来进行过滤？
+            # emmmm，总之后面测试的时候在考虑，暂时记录在此
+            for drawindexed_str in M_IniHelper.get_drawindexed_instanced_str_list(
+                submesh_model.drawcall_model_list,
+                obj_name_draw_offset_dict=drawib_model.obj_name_draw_offset,
+            ):
+                texture_override_ib_section.append(drawindexed_str)
 
             if len(self.blueprint_model.keyname_mkey_dict.keys()) != 0:
                 texture_override_ib_section.append("$active" + str(GlobalKeyCountHelper.generated_mod_number) + " = 1")
@@ -79,12 +92,12 @@ class ExportYYSLS(DrawIBExportBase):
             resource_vb_section.append("filename = " + buffer_folder_name + "/" + drawib_model.draw_ib + "-" + category_name + ".buf")
             resource_vb_section.new_line()
 
-        for part_name, ib_filename in drawib_model.PartName_IBBufferFileName_Dict.items():
-            ib_resource_name = drawib_model.PartName_IBResourceName_Dict.get(part_name, None)
+        for submesh_model in drawib_model.submesh_model_list:
+            ib_resource_name = self._get_submesh_ib_resource_name(submesh_model)
             resource_vb_section.append("[" + ib_resource_name + "]")
             resource_vb_section.append("type = Buffer")
             resource_vb_section.append("format = DXGI_FORMAT_R32_UINT")
-            resource_vb_section.append("filename = " + buffer_folder_name + "/" + ib_filename)
+            resource_vb_section.append("filename = " + buffer_folder_name + "/" + submesh_model.unique_str + "-Index.buf")
             resource_vb_section.new_line()
         ini_builder.append_section(resource_vb_section)
 
