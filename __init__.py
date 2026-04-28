9341a54e280cbd739a013d244c231e9f6e2bf7f6
暗灵QWQ <lan357351@gmail.com>
2026-04-28 12:26:13 +0800

    Bump version to 4.2.2

--- __init__.py
+++ __init__.py
@@ -29,7 +29,7 @@ bl_info = {
     "name": "TheHerta4",
     "description": "Blender Plugin of SSMT4",
     "blender": (4, 5, 0),
-    "version": (4, 2, 1),
+    "version": (4, 2, 2),
     "location": "View3D",
     "category": "Generic"
 }
